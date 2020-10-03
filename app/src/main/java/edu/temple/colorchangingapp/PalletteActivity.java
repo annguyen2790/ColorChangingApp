55f747d80facdf5a1f996161daf5cfcb40796e4a
An Nguyen <tuh09895@temple.edu>
2020-10-03 02:29:55 -0400

    Initial commit

---
+++ app/src/main/java/edu/temple/colorchangingapp/PalletteActivity.java
@@ -0,0 +1,14 @@
+package edu.temple.colorchangingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PalletteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pallete_main);
+    }
+}
