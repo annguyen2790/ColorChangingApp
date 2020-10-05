40d13baecf108d3dca251d1b3bcf6f109c723a58
An Nguyen <tuh09895@temple.edu>
2020-10-05 03:28:40 -0400

    Added intents and able to send data between activities

--- app/src/main/java/edu/temple/colorchangingapp/CanvasActivity.java
+++ app/src/main/java/edu/temple/colorchangingapp/CanvasActivity.java
@@ -1,15 +1,28 @@
 package edu.temple.colorchangingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.TextView;
 
 public class CanvasActivity extends AppCompatActivity {
-
+    TextView textView;
+    ConstraintLayout constraintLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_canvas);
+
+        textView = findViewById(R.id.textView_Color);
+        constraintLayout = findViewById(R.id.contrainst_Layout);
+
+        Intent toGet = getIntent();
+        String color = toGet.getStringExtra("Color");
+        textView.setText(color);
+
 
     }
 }
