7ff87111be94c29c277663f86fa399e887ab42bc
An Nguyen <tuh09895@temple.edu>
2020-10-05 02:27:36 -0400

    Set up the grid for the colors

--- app/src/main/java/edu/temple/colorchangingapp/PalletteActivity.java
+++ app/src/main/java/edu/temple/colorchangingapp/PalletteActivity.java
@@ -3,12 +3,33 @@ package edu.temple.colorchangingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class PalletteActivity extends AppCompatActivity {
-
+    GridView gridView;
+    ColorAdapter colorAdapter;
+    String [] colors = {"Red", "Blue" ,"Green"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pallete_main);
+        /*Initialize objects*/
+
+        gridView = findViewById(R.id.GridView_Color);
+        colorAdapter = new ColorAdapter(PalletteActivity.this, colors);
+        gridView.setAdapter(colorAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(PalletteActivity.this, "Clicked on Red", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
