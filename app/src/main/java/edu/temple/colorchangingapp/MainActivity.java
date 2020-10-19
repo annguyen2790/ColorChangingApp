cbad7b649570aa892809aa234ac99ea6bade7fd7
An Nguyen <tuh09895@temple.edu>
2020-10-19 03:10:07 -0400

    Finalize in removing hard coded string in canvas activity

--- app/src/main/java/edu/temple/colorchangingapp/MainActivity.java
+++ app/src/main/java/edu/temple/colorchangingapp/MainActivity.java
@@ -2,18 +2,14 @@ package edu.temple.colorchangingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-
-public class PalletteActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
     GridView gridView;
     ColorAdapter colorAdapter;
 
@@ -31,16 +27,14 @@ public class PalletteActivity extends AppCompatActivity {
         /*Testing*/
 
         gridView = findViewById(R.id.GridView_Color);
-        colorAdapter = new ColorAdapter(PalletteActivity.this, colours);
+        colorAdapter = new ColorAdapter(MainActivity.this, colours);
         gridView.setAdapter(colorAdapter);
 
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            Intent intent = new Intent(PalletteActivity.this, CanvasActivity.class);
+
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                //Toast.makeText(PalletteActivity.this, adapterView.getItemAtPosition(i).toString(), Toast.LENGTH_SHORT).show();
-                intent.putExtra("Color", adapterView.getItemAtPosition(i).toString());
-                startActivity(intent);
+
             }
         });
 
